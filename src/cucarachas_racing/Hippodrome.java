e07bdb30fede0e054a9dc37f462bd0d50bd4ea31
kd <erm.melnik@gmail.com>
2025-06-08 12:54:56 +0200

    Selection menu code update

--- src/cucarachas_racing/Hippodrome.java
+++ src/cucarachas_racing/Hippodrome.java
@@ -6,10 +6,12 @@ import java.util.Scanner;
 
 public class Hippodrome {
     public static void main(String[] args) {
-        Integer[] values = new Integer[2];
-        menu(values);
-        int participant = values[0];
-        int rounds = values[1];
+
+        System.out.print("Set amount of participant: ");
+        int participant = menu(2);
+        System.out.print("Set amount of rounds: ");
+        int rounds = menu(1);
+
 
         System.out.println("Participants: " + participant + ", Rounds: " + rounds);
         System.out.println("Starting the race!\n");
@@ -32,41 +34,22 @@ public class Hippodrome {
         }
     }
 
-    public static Integer[] menu(Integer[] values) {
+    public static int menu(int minValue) {
         Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Set amount of participant: ");
+        int result;
         while (true) {
             if (scanner.hasNextInt()) {
-                values[0] = scanner.nextInt();
-                if (values[0] >= 2) {
+                result = scanner.nextInt();
+                if (result >= minValue) {
                     break;
                 } else {
-                    System.out.println("Not less than 2!");
+                    System.out.printf("Not less than %d! ", minValue);
                 }
             } else {
                 System.out.println("Invalid input! Please enter a number.");
                 scanner.next();
             }
         }
-
-        System.out.print("Set amount of rounds: ");
-        while (true) {
-            if (scanner.hasNextInt()) {
-                values[1] = scanner.nextInt();
-                if (values[1] >= 1) {
-                    break;
-                } else {
-                    System.out.println("Set at least 1!");
-                }
-            } else {
-                System.out.println("Invalid input! Please enter a number.");
-                scanner.next();
-            }
-        }
-
-        scanner.close();
-        return values;
+        return result;
     }
-
 }
